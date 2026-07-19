194b50d90cd5fa6c2cff8995541d50482bf3d76e
Temuulen Enkhtamir <temuulen.enkhtamir@dukekunshan.edu.cn>
2026-07-19 10:34:45 +0800

    Prioritize TS highlights over lsp

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -3,5 +3,7 @@ require("nvchad.options")
 -- add yours here!
 vim.opt.relativenumber = true
 vim.o.winborder = "rounded"
+-- Prefer Tree-sitter syntax colors over LSP semantic token colors.
+vim.hl.priorities.treesitter = 130
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
