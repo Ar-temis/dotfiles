5e6683a1d4958a502563ee6b63f3c5c62b9c8786
Temuulen Enkhtamir <temuulen.enkhtamir@dukekunshan.edu>
2026-08-31 22:44:58 +0800

    updates

--- .config/hypr/monitors.lua
+++ .config/hypr/monitors.lua
@@ -6,7 +6,7 @@
 hl.env("GDK_SCALE", "1")
 
 -- Fallback for any monitor not listed below.
-hl.monitor({ output = "", mode = "preferred", position = "auto", scale = "auto" })
+hl.monitor({ output = "", mode = "preferred", position = "auto", scale = 1 })
 
 hl.monitor({ output = "HDMI-A-1", mode = "1920x1080@60", position = "0x0", scale = 1 })
 hl.monitor({ output = "eDP-2", mode = "2560x1440@240", position = "1920x0", scale = 1 })
